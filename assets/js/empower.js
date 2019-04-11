0a500ede55b101f6fbc8875b023bf0ed76749fa0
Emma Campbell <emma@luminate.one>
2019-04-12 11:30:22 +1200

    Quill Editor toolbar edited
    to remove features unsupported by the PHP Quill Renderer package

--- assets/js/empower.js
+++ assets/js/empower.js
@@ -25,21 +25,14 @@ $(document).on('ajaxSuccess', '#post-form', function(event, context, data) {
 
 var toolbarOptions = [
     ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-    ['blockquote', 'code-block'],
+    ['blockquote'],
 
     [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
     [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-    [{ 'direction': 'rtl' }],                         // text direction
 
     [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
     [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
     [ 'link', 'image', 'video', 'formula' ],          // add's image support
-    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-    [{ 'font': [] }],
-    [{ 'align': [] }],
-
     ['clean']                                         // remove formatting button
 ];
 
