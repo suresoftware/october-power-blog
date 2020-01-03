c338e6b631a23d5bc2ec643660611f5edea2177d
Nicholas Humphries <nicholas.humphries0@gmail.com>
2020-01-04 11:19:42 +1300

    Fixed blog creation bug

--- controllers/QuillController.php
+++ controllers/QuillController.php
@@ -12,6 +12,10 @@ class QuillController extends Controller
 
     public function storeDelta(Request $request)
     {
+        if($request->id == 'create'){
+            return response()->json(['message' => 'post not yet created'], 422);
+        }
+
         $post = Post::find($request->id);
         $post->powerblog_delta = $request->doc;
         $post->save();
