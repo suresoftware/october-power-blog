59c23a8d7510f10d92c584ea78bf2b63f07cc31a
Kodie Upton <kodie@luminate.one>
2020-08-20 15:25:11 +1200

    Added doc blocks

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Routing\Controller;
 use Rainlab\Blog\Models\Post;
-use Cms\Classes\Page as CmsPage;
 use SureSoftware\PowerBlog\Models\Settings;
 
 /**
@@ -10,18 +9,26 @@ use SureSoftware\PowerBlog\Models\Settings;
  */
 class BlogController extends Controller
 {
+    /**
+     * Return latest 3 blog posts.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getPublished()
     {
+        // Check whether API is enabled.
         if (Settings::get('enable_api', false) != true) {
             return response()->json('Currently unavailable.', 404);
         }
 
+        // Return the latest 3 blog posts
         $posts = Post::isPublished()
             ->orderBy('published_at', 'DESC')
             ->limit(3)
             ->get();
 
 
+        // Return blog page.
         $baseUrl = Settings::get('blog_page_slug');
         if ($baseUrl !== '' && substr($baseUrl, -1) !== '/') {
             $baseUrl .= '/';
@@ -29,6 +36,7 @@ class BlogController extends Controller
 
         $blogPosts = [];
         if ($posts) {
+            // Create a new array based on post data.
             $posts->each(function ($post) use (&$blogPosts, $baseUrl) {
                 $data = [
                     'title' => $post->title,
